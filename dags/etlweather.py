2d50e6191cd964a96f9e489708b35d0225e59746
bibek tulachan <bibek4@MacBook-Pro.local>
2025-11-16 15:18:17 +0000

    Add comments

--- dags/etlweather.py
+++ dags/etlweather.py
@@ -1,7 +1,7 @@
-from airflow import DAG
-from airflow.providers.http.hooks.http import HttpHook
-from airflow.providers.postgres.hooks.postgres import PostgresHook
-from airflow.decorators import task
+from airflow import DAG # We need this library 
+from airflow.providers.http.hooks.http import HttpHook # This hooks library from airflow helps to read from the API
+from airflow.providers.postgres.hooks.postgres import PostgresHook # This helps to push our data to postgres sql. Similarly, there will be a separate hooks to push to S3 or mysql
+from airflow.decorators import task # This helps to create tasks inside our DAG
 from airflow.utils.dates import days_ago
 import requests
 import json
@@ -9,34 +9,35 @@ import json
 # Latitude and longitude for the desired location (London in this case)
 LATITUDE = '51.5074'
 LONGITUDE = '-0.1278'
-POSTGRES_CONN_ID='postgres_default'
-API_CONN_ID='open_meteo_api'
+POSTGRES_CONN_ID='postgres_default' # This will be our connection id
+API_CONN_ID='open_meteo_api' # Connection name
 
+# Basic setup that is required in airflow
 default_args={
     'owner':'airflow',
     'start_date':days_ago(1)
 }
 
 ## DAG
-with DAG(dag_id='weather_etl_pipeline',
+with DAG(dag_id='weather_etl_pipeline', # DAG name
          default_args=default_args,
          schedule_interval='@daily',
          catchup=False) as dags:
     
-    @task()
-    def extract_weather_data():
+    @task() # We have already imported this library
+    def extract_weather_data(): # Task function
         """Extract weather data from Open-Meteo API using Airflow Connection."""
 
-        # Use HTTP Hook to get connection details from Airflow connection
+        # Use HTTP Hook to get connection details from Airflow connection (configuration in airflow)
 
-        http_hook=HttpHook(http_conn_id=API_CONN_ID,method='GET')
+        http_hook=HttpHook(http_conn_id=API_CONN_ID,method='GET') # http_hook is our variable
 
         ## Build the API endpoint
         ## https://api.open-meteo.com/v1/forecast?latitude=51.5074&longitude=-0.1278&current_weather=true
         endpoint=f'/v1/forecast?latitude={LATITUDE}&longitude={LONGITUDE}&current_weather=true'
 
-        ## Make the request via the HTTP Hook
-        response=http_hook.run(endpoint)
+        ## Make the request via the HTTP Hook (Hit that end point)
+        response=http_hook.run(endpoint) # with the endpoint we should be able to get a response
 
         if response.status_code == 200:
             return response.json()
@@ -44,7 +45,7 @@ with DAG(dag_id='weather_etl_pipeline',
             raise Exception(f"Failed to fetch weather data: {response.status_code}")
         
     @task()
-    def transform_weather_data(weather_data):
+    def transform_weather_data(weather_data): # this function is transforming the data
         """Transform the extracted weather data."""
         current_weather = weather_data['current_weather']
         transformed_data = {
@@ -58,7 +59,7 @@ with DAG(dag_id='weather_etl_pipeline',
         return transformed_data
     
     @task()
-    def load_weather_data(transformed_data):
+    def load_weather_data(transformed_data): # This function is pushing the transformed data to PostgresSQL
         """Load transformed data into PostgreSQL."""
         pg_hook = PostgresHook(postgres_conn_id=POSTGRES_CONN_ID)
         conn = pg_hook.get_conn()
